5a6c0432119884f12632f96a9314b9ee92e6a270
Hoodh Waleedh <hoodhwaleedh@gmail.com>
2026-09-06 00:57:32 -0400

    mvp for entities made

--- src/main/java/com/concertn/localbands/domain/Events.java
+++ src/main/java/com/concertn/localbands/domain/Events.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -21,6 +22,27 @@ public class Events {
     @Column(name="id",nullable = false,updatable = false)
     private UUID id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="bands_id")
+    private Bands band;
 
+    @Column(name="likes",nullable = false)
+    private Integer likes = 0;
 
+    //Location:
+    @Column(name="latitude")
+    private double latitude;
+
+    @Column(name="longitude")
+    private double longitude;
+
+    //TODO: data for storing promotion pictures?
+
+    @Column(name="date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "doors_open")
+    private LocalDateTime doorsOpen;
+
+    //don't need doors closed (unnecessary for now)
 }
