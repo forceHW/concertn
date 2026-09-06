487db7a96f69c37ab85d1f1ae22ad2bd009a22d9
Hoodh Waleedh <hoodhwaleedh@gmail.com>
2026-09-05 21:09:18 -0400

    spring initializr!

---
+++ src/main/java/com/concertn/localbands/LocalbandsApplication.java
@@ -0,0 +1,13 @@
+package com.concertn.localbands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalbandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalbandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concertn/localbands/LocalbandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concertn.localbands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalbandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
